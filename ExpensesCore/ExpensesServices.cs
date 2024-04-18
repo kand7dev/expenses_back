439e9dc8a57e8bfafa38b9a436c91ec83aaa3e2b
kand7dev <kand7dev@gmail.com>
2024-04-18 15:37:33 +0300

    add expenses core project
    This project contains methods that use our dbcontext and will be called when we hit an end point

---
+++ ExpensesCore/ExpensesServices.cs
@@ -0,0 +1,12 @@
+﻿using ExpensesDb;
+
+namespace ExpensesCore
+{
+    public class ExpensesServices: IExpensesServices
+    {
+        private ExpenseDbContext _context;
+        public ExpensesServices(ExpenseDbContext context) => _context = context;
+
+        public List<Expense> GetExpenses() => _context.Expenses.ToList();
+    }
+}
