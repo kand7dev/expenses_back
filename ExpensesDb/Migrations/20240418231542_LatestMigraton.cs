11bc39a70a2a43905f56e8177562c247d514093c
kand7dev <kand7dev@gmail.com>
2024-04-19 22:23:52 +0300

    perform new migrations

--- ExpensesDb/Migrations/20240418231542_LatestMigraton.cs
+++ ExpensesDb/Migrations/20240418231542_LatestMigraton.cs
@@ -5,17 +5,11 @@
 namespace ExpensesDb.Migrations
 {
     /// <inheritdoc />
-    public partial class user_table : Migration
+    public partial class LatestMigraton : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "UserId",
-                table: "Expenses",
-                type: "int",
-                nullable: true);
-
             migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
@@ -24,43 +18,48 @@ namespace ExpensesDb.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Users", x => x.Id);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Expenses_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Expenses_UserId",
                 table: "Expenses",
                 column: "UserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Expenses_Users_UserId",
-                table: "Expenses",
-                column: "UserId",
-                principalTable: "Users",
-                principalColumn: "Id");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Expenses_Users_UserId",
-                table: "Expenses");
+            migrationBuilder.DropTable(
+                name: "Expenses");
 
             migrationBuilder.DropTable(
                 name: "Users");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Expenses_UserId",
-                table: "Expenses");
-
-            migrationBuilder.DropColumn(
-                name: "UserId",
-                table: "Expenses");
         }
     }
 }
