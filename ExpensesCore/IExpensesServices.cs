06f392c0ce6469947d2f1f99ee42c2c5d2d63571
kand7dev <kand7dev@gmail.com>
2024-04-22 23:50:28 +0300

    re-implement interfaces

--- ExpensesCore/IExpensesServices.cs
+++ ExpensesCore/IExpensesServices.cs
@@ -1,19 +1,14 @@
 ﻿
 using ExpensesCore.DTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExpensesCore
 {
     public interface IExpensesServices
     {
         List<Expense> GetExpenses();
-        Expense? GetExpense(int id);
+        Expense GetExpense(int id);
         Expense CreateExpense(ExpensesDb.Expense expense);
         bool DeleteExpense(Expense expense);
-        Expense? EditExpense(Expense expense);
+        Expense EditExpense(Expense expense);
     }
 }
