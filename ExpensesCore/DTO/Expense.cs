00016e305a675d25504b46637531ffdc8ef328c2
kand7dev <kand7dev@gmail.com>
2024-04-23 00:11:16 +0300

    use required in model and dto

--- ExpensesCore/DTO/Expense.cs
+++ ExpensesCore/DTO/Expense.cs
@@ -4,7 +4,7 @@ namespace ExpensesCore.DTO
     public class Expense
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public required string Description { get; set; }
         public double Amount { get; set; }
 
         public static explicit operator Expense(ExpensesDb.Expense e) => new Expense
