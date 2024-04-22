da545b3541df998f1304abd744fbe46ecf738926
kand7dev <kand7dev@gmail.com>
2024-04-22 23:51:32 +0300

    make Description nullable

--- ExpensesCore/DTO/Expense.cs
+++ ExpensesCore/DTO/Expense.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ExpensesCore.DTO
 {
     public class Expense
     {
         public int Id { get; set; }
-        public required string Description { get; set; }
+        public string Description { get; set; }
         public double Amount { get; set; }
 
         public static explicit operator Expense(ExpensesDb.Expense e) => new Expense
