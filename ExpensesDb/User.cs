dba86dd2d5839992c9022d4217506f25b1d12c5b
kand7dev <kand7dev@gmail.com>
2024-04-23 01:18:28 +0300

    some fields must be nullable

--- ExpensesDb/User.cs
+++ ExpensesDb/User.cs
@@ -10,9 +10,9 @@ namespace ExpensesDb
     public class User
     {
         public int Id { get; set; }
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-        public required string Email { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
 
     }
 }
