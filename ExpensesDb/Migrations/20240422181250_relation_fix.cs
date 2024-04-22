127b1b46979c5e35b5d7d6636b35441ca5450067
kand7dev <kand7dev@gmail.com>
2024-04-22 23:46:06 +0300

    perform new migrations

---
+++ ExpensesDb/Migrations/20240422181250_relation_fix.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensesDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class relation_fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_UserId",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Expenses",
+                newName: "FK_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_UserId",
+                table: "Expenses",
+                newName: "IX_Expenses_FK_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_FK_UserId",
+                table: "Expenses",
+                column: "FK_UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_FK_UserId",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "FK_UserId",
+                table: "Expenses",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_FK_UserId",
+                table: "Expenses",
+                newName: "IX_Expenses_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_UserId",
+                table: "Expenses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- ExpensesDb/User.cs
+++ ExpensesDb/User.cs
@@ -10,8 +10,8 @@ namespace ExpensesDb
     public class User
     {
         public int Id { get; set; }
-        public required string Username{ get; set; }
-        public required string Password { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string? Email { get; set; }
 
     }
