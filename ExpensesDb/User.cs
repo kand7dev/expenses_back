9cb5e4a5bf4549ef19e0c03440aa8352c329d649
kand7dev <kand7dev@gmail.com>
2024-04-23 22:33:44 +0300

    add externalid and externaltype to database

--- ExpensesDb/User.cs
+++ ExpensesDb/User.cs
@@ -13,6 +13,8 @@ namespace ExpensesDb
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? Email { get; set; }
+        public string? ExternalId { get; set; }
+        public string? ExternalType { get; set; }
 
     }
 }
