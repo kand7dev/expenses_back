0121e0bc87af2c175ec8c76d4896a81448dd59d9
kand7dev <kand7dev@gmail.com>
2024-04-24 23:27:01 +0300

    add the authorized end point and implement the get method.

---
+++ ExpensesBackend/Controllers/StatisticsController.cs
@@ -0,0 +1,23 @@
+﻿using ExpensesCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesBackend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsServices _statisticsServices;
+        public StatisticsController(IStatisticsServices statisticsServices)
+        {
+            _statisticsServices = statisticsServices;
+        }
+        [HttpGet]
+        public IActionResult GetExpenseAmountPerCategory()
+        {
+            return Ok(_statisticsServices.GetExpenseAmountPerCategory());
+        }
+    }
+}
